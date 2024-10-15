42c89ddb6637947768090228695f3e0b83c000cf
Aanchal <anchal.verma@ibyteinfomatics.in>
2024-10-15 17:51:44 +0530

    add

---
+++ dataType/dataTypes.js
@@ -0,0 +1,15 @@
+let marks = 12.5;
+let marks2 = "string";
+let marks3 = true;
+let adc;
+let b = null
+console.log(marks);
+console.log(marks2);
+console.log(marks3);
+console.log(adc); //undefined
+
+let market = 87689567985679857698567987698780;
+console.log(market);
+console.log(typeof(null));
+console.log(typeof(adc));
+
---
+++ variables/variables.js
@@ -0,0 +1,30 @@
+//----------------------------------------var keyword----------------------------------------------
+var a = 10;
+
+function add(){
+    var a = 20
+    console.log(a);
+    
+}
+
+console.log(a);
+add()
+console.log(a);
+
+//--------------------------------------let kryword-----------------------------------------------
+
+{
+    let b = 10;
+    console.log(b);
+    
+}
+let b = 12
+console.log(b);
+b = 122
+console.log(b);
+
+//---------------------------------------const keyword---------------------------------------------
+const c = 10
+console.log(c);
+// c = 12 , const c = 123 not possible
+
